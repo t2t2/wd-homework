ee18560cd3b5035b5fa234e86ef162e1e913910e
t2t2 <taavot@gmail.com>
2019-11-22 12:55:03 +0200

    Links on homepage just in case

--- src/router/views/home.vue
+++ src/router/views/home.vue
@@ -1,5 +1,14 @@
 <template>
 	<div class="home">
-		←
+		<p>
+			<router-link :to="{name: 'tags.list'}">
+				Task 2 - Tags list & details
+			</router-link>
+		</p>
+		<p>
+			<router-link :to="{name: 'projects.view'}">
+				Task 3 - Project details
+			</router-link>
+		</p>
 	</div>
 </template>
